e305d7f396447a66a9837983a6beb92c058135c1
Joss <jossappletonfox@protonmail.com>
2022-04-13 11:30:28 +0100

    Populate movie list

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log(movies);
+const movieList = document.getElementById('movie-list');
+
+for (const movie of movies) {
+	const id = movie.imdbID;
+	const poster = movie.poster;
+	const title = movie.title;
+	
+	const li = document.createElement('li');
+	li.setAttribute('id', id);
+
+	const a = document.createElement('a');
+	a.setAttribute('href', `https://imdb.com/title/${id}`);
+
+	const img = document.createElement('img');
+	img.setAttribute('src', poster);
+	img.setAttribute('alt', title);
+
+	a.appendChild(img);
+	li.appendChild(a);
+	movieList.appendChild(li);
+}
